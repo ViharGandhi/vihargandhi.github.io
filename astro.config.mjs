d62c2f798ea96974ebd7ff8a31fcbc28db00f206
ViharGandhi <97725643+ViharGandhi@users.noreply.github.com>
2025-12-01 22:11:28 +0530

    Delete astro.config.mjs

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  site: 'https://vihargandhi.github.io',
-  build: {
-    format: 'file' // makes /blog-post.html → /blog-post (clean URLs)
-  }
-});
